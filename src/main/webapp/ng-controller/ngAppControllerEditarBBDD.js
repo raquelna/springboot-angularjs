6b35cbcd7f5eececd45cf6b96cc6a42c93878d90
Lenovo <Lenovo@LENOVO-T420>
2017-09-20 13:51:39 +0200

    modificacion controller editar

--- src/main/webapp/ng-controller/ngAppControllerEditarBBDD.js
+++ src/main/webapp/ng-controller/ngAppControllerEditarBBDD.js
@@ -3,4 +3,21 @@ app.controller('ngAppControllerEditarBBDD',
   function ($scope, $http, $timeout, utilFactory)
   {
 	
-  }
+	(function()
+			{
+		
+			
+	 $http.post('/getItems',
+		      {
+		        table: 'valor enviado por POST utilizando el modelo TESTMODEL'
+		      })
+		      .then(function (response)
+		      {
+		       var data = response.data;
+		      
+		       $scope.bbdd = data;
+		      });
+			})();
+	
+	
+  }]);
